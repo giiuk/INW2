79d302b3350aa92d92552e62228d7405253a37b2
Logon Aluno <logonrmlocal@fiap.com.br>
2023-05-09 13:42:30 -0300

    exemplos de códigos em POO

---
+++ pooAnimais/cdBanco.js
@@ -0,0 +1,35 @@
+class Conta{
+    constructor(numero,saldo,ativa){
+        this.numero= numero;
+        this.saldo= saldo;
+        this.ativa=ativa
+    }
+    credito(valor){
+        this.saldo = this.saldo + valor
+    }
+    debito(valor){
+        if(valor<0){
+            console.log("Impossivel fazer, número negativo...")
+        }
+        else if(this.saldo<valor){
+            console.log("impossivel realizar, saldo indisponivel...")
+        }
+        else{
+            this.saldo = this.saldo - valor
+        }
+        
+
+    }
+}
+
+//programa principal
+
+let conta1 = new Conta(15,0,true)
+
+console.log("Saldo atual conta: " +conta1.saldo)
+conta1.credito(100)
+console.log("Saldo atual da conta: "+conta1.saldo)
+conta1.debito(50)
+console.log("Saldo atual conta: "+conta1.saldo)
+conta1.debito(60)
+console.log("Saldo atual da conta: " +conta1.saldo)
